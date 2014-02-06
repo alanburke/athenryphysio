e24618dfb4dbc70d6c23d4198e1eb96e314d1a72
Alan Burke <alan@cycletraffic.com>
2014-02-06 17:46:49 +0100

    Update Map point

--- javascript/map.init.js
+++ javascript/map.init.js
@@ -5,6 +5,6 @@ L.tileLayer('http://{s}.tile.cloudmade.com/49a951fe09cb4104ae574520ac127b6e/997/
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
 }).addTo(map);
-var marker = L.marker([53.299436, -8.756855]).addTo(map);
+var marker = L.marker([53.299676124615054, -8.75533914368134]).addTo(map);
 marker.bindPopup("<strong>Athenry Physiotheraphy</strong>").openPopup();
 });
