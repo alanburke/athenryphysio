8450588f751c9ebc038ceaa3cf7fb136c6842db5
Alan Burke <alan@cycletraffic.com>
2014-08-25 21:18:58 +0100

    Change Map Provider to Openstreetmap

--- javascript/map.init.js
+++ javascript/map.init.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 
 var map = L.map('map').setView([53.299436, -8.756855], 14);
-L.tileLayer('http://{s}.tile.cloudmade.com/49a951fe09cb4104ae574520ac127b6e/997/256/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery <a href="http://cloudmade.com">CloudMade</a>',
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
     maxZoom: 18
 }).addTo(map);
 var marker = L.marker([53.299676124615054, -8.75533914368134]).addTo(map);
